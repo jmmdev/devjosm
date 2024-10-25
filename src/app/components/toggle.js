89528f2ed3617d4d1f989a8bf9045742eb5fb61c
unknown <devjosm@gmail.com>
2024-10-25 15:09:14 +0200

    Toggle adjust

--- src/app/components/toggle.js
+++ src/app/components/toggle.js
@@ -11,9 +11,9 @@ export default function Toggle({language, setLanguage}) {
     return (
         <div className="absolute top-4 right-4 z-50 flex items-center gap-2">
             <p className={`text-lg ${language === 'en' ? "text-zinc-500" : "text-zinc-100"}`}>ES</p>
-            <button className="group w-12 h-6 border-2 border-zinc-500 hover:border-zinc-100 rounded-full" onClick={() => switchLanguage()}>
-                <div className={`flex ${language === 'es' ? "justify-start" : "justify-end"} bg-violet-500/30 w-full h-full p-0.5 rounded-full`}>
-                    <div className="bg-violet-300 h-full aspect-square rounded-full" />
+            <button className="group w-10 h-6 border-2 border-zinc-500 hover:border-zinc-100 rounded-full" onClick={() => switchLanguage()}>
+                <div className={`flex duration-150 bg-violet-500/30 w-full h-full p-0.5 rounded-full`}>
+                    <div className={`bg-violet-300 w-1/2 h-full rounded-full ${language === 'es' ? "translate-x-0 group-active:translate-x-full" : "translate-x-full group-active:translate-x-0"} transition-all`} />
                 </div>
             </button>
             <p className={`text-lg ${language === 'en' ? "text-zinc-100" : "text-zinc-500"}`}>EN</p>
