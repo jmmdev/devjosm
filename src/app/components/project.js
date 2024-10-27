75c728fc54dedab8860f346581cc8085bc6f082a
unknown <devjosm@gmail.com>
2024-10-27 05:07:12 +0100

    Projects updated

--- src/app/components/project.js
+++ src/app/components/project.js
@@ -16,21 +16,21 @@ export default function Project({project, language}) {
 
     return (
         <li className="mb-12">
-            <div className="group relative grid gap-6 transition-all sm:grid-cols-8 sm:gap-8 md:gap-6 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-                <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition lg:-inset-x-6 lg:block lg:group-hover:bg-violet-900/20 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+            <a className="lg:bg-transparent rounded-lg lg:rounded-none group relative grid gap-6 transition-all sm:grid-cols-8 sm:gap-8 md:gap-6 lg:hover:!opacity-100 lg:group-hover/list:opacity-50" href={project.url} target="_blank" rel="noreferrer noopener" aria-label={`${project.alt} (opens in a new tab)`}>
+                <div className="absolute -inset-y-4 z-0 hidden rounded-md transition lg:-inset-x-6 lg:block lg:group-hover:bg-violet-900/20 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
                 <div className="z-10 sm:order-2 sm:col-span-6">
                     <h3>
-                        <a className="inline-flex items-baseline font-medium leading-tight text-zinc-200 hover:text-violet-300 focus-visible:text-violet-300 group/link text-lg" href={project.url} target="_blank" rel="noreferrer noopener" aria-label={`${project.alt} (opens in a new tab)`}>
+                        <div className="inline-flex items-baseline font-medium leading-tight text-zinc-200 hover:text-violet-300 focus-visible:text-violet-300 group/link text-lg">
                             <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
                             <span>
                             {project.name}
                             <span className="inline-block">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="hidden h-4 w-4 shrink-0 transition-transform group-hover/link:inline-block ml-1" aria-hidden="true">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block lg:hidden h-4 w-4 shrink-0 transition-transform group-hover/link:inline-block ml-1" aria-hidden="true">
                                     <path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path>
                                 </svg>
                             </span>
                             </span>
-                        </a>
+                        </div>
                     </h3>
                     <p className="mt-2 leading-normal">{project.description[language]}</p>
 
@@ -39,7 +39,7 @@ export default function Project({project, language}) {
                     </ul>
                 </div>
                 <Image alt={project.alt} loading="lazy" width="200" height="48" decoding="async" data-nimg="1" className="rounded border-2 border-zinc-200/20 transition group-hover:border-zinc-200/50 sm:order-1 sm:col-span-2 sm:translate-y-1" src={`/assets/images/${project.ref}.png`} style={{color: "transparent"}} />
-            </div>
+            </a>
         </li>
     )
 }
