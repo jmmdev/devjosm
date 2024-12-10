d969961eab2a5ae2f3290728b46296af64e8b10d
unknown <devjosm@gmail.com>
2024-12-10 02:28:33 +0100

    Gamescript updated

--- src/app/components/project.js
+++ src/app/components/project.js
@@ -34,7 +34,7 @@ export default function Project({project, language}) {
     }
 
     return (
-        <li className="mb-12">
+        <li className="py-6">
             <a className="lg:bg-transparent rounded-lg lg:rounded-none group relative grid gap-6 transition-all sm:grid-cols-8 sm:gap-8 md:gap-6 lg:hover:!opacity-100 lg:group-hover/list:opacity-50" href={project.url} target="_blank" rel="noreferrer noopener" aria-label={`${project.alt} (opens in a new tab)`}>
                 <div className="absolute -inset-y-4 z-0 hidden rounded-md transition lg:-inset-x-6 lg:block lg:group-hover:bg-violet-900/20 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
                 <div className="z-10 sm:order-2 sm:col-span-6">
